e8f6e8ceb7b728aa8cb7ca8321f1ff2ad399588b
rodrigonm <rdrg.n.magalhaes@gmail.com>
2018-03-29 15:18:42 -0300

    set passenger-restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,10 +6,12 @@ set :repo_url, "git@github.com:matcheus/Client-Manager.git"
 
 set :passenger_restart_with_touch, true
 
-# set :puma_threads,    [4, 16]
-
-set :deploy_to, "/home/deployer/client_manager"
+# Default branch is :master
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/var/www/client-manager"
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
 set :rvm1_map_bins, %w{rake gem bundle ruby}
 
-append :linked_files, "config/database.yml", "config/secrets.yml"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
